d5912b1780ea3a5749a4c94051d9c1fee92241d2
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-22 16:47:49 -0700

    updates serializer to match spec

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -7,8 +7,12 @@ describe 'merchant request' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
-    parsed = JSON.parse(response.body)["data"]
+    parsed = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(parsed.count).to eq(3)
+    expect(parsed.first).to have_key(:id)
+    expect(parsed.first).to have_key(:type)
+    expect(parsed.first).to have_key(:attributes)
+    expect(parsed.first[:attributes]).to have_key(:name)
   end
 end

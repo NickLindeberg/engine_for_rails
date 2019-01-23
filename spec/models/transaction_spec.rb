a1d908151139afee851b7e20efa9f124b2b32433
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-23 13:18:58 -0700

    test setup

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  it { should belong_to(:invoice)}
+
+  describe 'validations' do
+    it { should validate_presence_of(:credit_card_number)}
+    it { should validate_presence_of(:result)}
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:invoice)}
+  end
+
+end

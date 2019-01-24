a3f8033bf598d4db493ba6ebe7cbabef515a3fca
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-24 13:16:27 -0700

    fixes relationships through joins table

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,12 @@ class Invoice < ApplicationRecord
   belongs_to :merchant
   belongs_to :customer
   has_many :invoice_items
+  has_many :transactions
+  has_many :items, through: :invoice_items
+
   validates_presence_of :status
+
+  scope :successful_invoices, -> { joins(:transactions).where("transactions.result = 'successful'") }
 
   def self.best_day(item_id)
     Invoice.select("invoices.created_at, sum(invoice_items.quantity) AS best_day")
@@ -13,4 +18,12 @@ class Invoice < ApplicationRecord
            .limit(1)
            .first
   end
+
+
+
+  def self.successful
+    joins(:transactions)
+      .where("transactions.result = 'successful'")
+  end
+
 end

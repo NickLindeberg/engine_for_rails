38e5e9965a07f73b8f4a142e1ae69e2065ffeb34
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-26 13:57:47 -0700

    adds updated and created at to test files

--- spec/requests/api/v1/customers/customer_find_spec.rb
+++ spec/requests/api/v1/customers/customer_find_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'customer find request' do
   it 'finds customer by id' do
-    create_list(:customer, 2)
-    cust = Customer.first
+    cust_1 = create(:customer)
+    cust_2 = create(:customer)
 
-    get "/api/v1/customers/find?#{cust.id}"
+    get "/api/v1/customers/find?#{cust_1.id}"
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
@@ -15,14 +15,14 @@ describe 'customer find request' do
     expect(data).to have_key(:id)
     expect(data).to have_key(:type)
     expect(data).to have_key(:attributes)
-    expect(data[:id]).to eq(cust.id.to_s)
+    expect(data[:id]).to eq(cust_1.id.to_s)
   end
 
   it 'finds customer by first_name' do
-    create_list(:customer, 2)
-    cust = Customer.first
+    cust_1 = create(:customer, first_name: "Joe")
+    cust_2 = create(:customer)
 
-    get "/api/v1/customers/find?#{cust.first_name}"
+    get "/api/v1/customers/find?#{cust_1.first_name}"
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
@@ -32,14 +32,14 @@ describe 'customer find request' do
     expect(data).to have_key(:id)
     expect(data).to have_key(:type)
     expect(data).to have_key(:attributes)
-    expect(data[:id]).to eq(cust.id.to_s)
+    expect(data[:id]).to eq(cust_1.id.to_s)
   end
 
   it 'finds customer by last_name' do
-    create_list(:customer, 2)
-    cust = Customer.first
+    cust_1 = create(:customer, last_name: "Bob")
+    cust_2 = create(:customer)
 
-    get "/api/v1/customers/find?#{cust.last_name}"
+    get "/api/v1/customers/find?#{cust_1.last_name}"
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
@@ -49,14 +49,14 @@ describe 'customer find request' do
     expect(data).to have_key(:id)
     expect(data).to have_key(:type)
     expect(data).to have_key(:attributes)
-    expect(data[:id]).to eq(cust.id.to_s)
+    expect(data[:id]).to eq(cust_1.id.to_s)
   end
 
   it 'finds customer by created_at' do
-    create_list(:customer, 2)
-    cust = Customer.first
+    cust_1 = create(:customer, created_at: "2012-03-27 14:54:09 UTC")
+    cust_2 = create(:customer)
 
-    get "/api/v1/customers/find?#{cust.created_at}"
+    get "/api/v1/customers/find?#{cust_1.created_at}"
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
@@ -66,14 +66,14 @@ describe 'customer find request' do
     expect(data).to have_key(:id)
     expect(data).to have_key(:type)
     expect(data).to have_key(:attributes)
-    expect(data[:id]).to eq(cust.id.to_s)
+    expect(data[:id]).to eq(cust_1.id.to_s)
   end
 
   it 'finds customer by updated_at' do
-    create_list(:customer, 2)
-    cust = Customer.first
+    cust_1 = create(:customer, updated_at: "2012-03-27 14:54:09 UTC")
+    cust_2 = create(:customer)
 
-    get "/api/v1/customers/find?#{cust.updated_at}"
+    get "/api/v1/customers/find?#{cust_1.updated_at}"
 
     expect(response).to be_successful
     parsed = JSON.parse(response.body, symbolize_names: true)
@@ -83,6 +83,6 @@ describe 'customer find request' do
     expect(data).to have_key(:id)
     expect(data).to have_key(:type)
     expect(data).to have_key(:attributes)
-    expect(data[:id]).to eq(cust.id.to_s)
+    expect(data[:id]).to eq(cust_1.id.to_s)
   end
 end
--- spec/requests/api/v1/merchants/merchant_find_spec.rb
+++ spec/requests/api/v1/merchants/merchant_find_spec.rb
@@ -41,4 +41,44 @@ describe 'merchant find' do
     expect(data[:id]).to_not eq(merchant_2.id.to_s)
   end
 
+  it 'can find a merchant by created_at' do
+    merchant_1 = create(:merchant, created_at: "2012-03-27 14:54:09 UTC")
+    merchant_2 = create(:merchant)
+
+    get "/api/v1/merchants/find?#{merchant_1.created_at}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data.count).to eq(3)
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to have_key(:name)
+    expect(data[:id]).to eq(merchant_1.id.to_s)
+    expect(data[:id]).to_not eq(merchant_2.id.to_s)
+  end
+
+  it 'can find a merchant by updated_at' do
+    merchant_1 = create(:merchant, updated_at: "2012-03-27 14:54:09 UTC")
+    merchant_2 = create(:merchant)
+
+    get "/api/v1/merchants/find?#{merchant_1.updated_at}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data.count).to eq(3)
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to have_key(:name)
+    expect(data[:id]).to eq(merchant_1.id.to_s)
+    expect(data[:id]).to_not eq(merchant_2.id.to_s)
+  end
+
 end

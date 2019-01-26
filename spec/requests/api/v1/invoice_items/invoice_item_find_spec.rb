947f36c56ced8e1423929bc5d9cce992798291ad
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-26 13:13:05 -0700

    can find by ii quantity

--- spec/requests/api/v1/invoice_items/invoice_item_find_spec.rb
+++ spec/requests/api/v1/invoice_items/invoice_item_find_spec.rb
@@ -22,4 +22,26 @@ describe 'invoice_item find request' do
     expect(data[:id]).to eq(ii.id.to_s)
   end
 
+  it 'finds invoice_item by quantity' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+    item = create(:item, merchant_id: merchant.id)
+    ii_1 = create(:invoice_item, quantity: 4, invoice_id: invoice.id, item_id: item.id)
+    ii_2 = create(:invoice_item, invoice_id: invoice.id, item_id: item.id)
+
+    get "/api/v1/invoice_items/find?#{ii_1.quantity}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:id]).to eq(ii_1.id.to_s)
+    expect(data[:id]).to_not eq(ii_2.id.to_s)
+  end
+
 end

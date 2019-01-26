125ff3c64ec07fb324678714fe0199a2039e4452
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-26 14:11:25 -0700

    can find transaction by all attributes

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Merchants::FindController < ApplicationController
 
   def show
-    render json: MerchantSerializer.new(Merchant.find_by(item_params))
+    render json: MerchantSerializer.new(Merchant.find_by(merc_params))
   end
 
   private
 
-  def item_params
+  def merc_params
     params.permit("name", "created_at", "updated_at")
   end
 end
---
+++ spec/requests/api/v1/transactions/transaction_find_spec.rb
@@ -0,0 +1,78 @@
+require 'rails_helper'
+
+describe 'transaction find' do
+  it 'can find a transaction by id' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+    tran_1 = create(:transaction, invoice_id: invoice.id)
+    tran_2 = create(:transaction, invoice_id: invoice.id)
+
+    get "/api/v1/transactions/find?#{tran_1.id}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to have_key(:credit_card_number)
+    expect(data[:attributes]).to have_key(:credit_card_expiration_date)
+    expect(data[:attributes]).to have_key(:result)
+    expect(data[:attributes]).to have_key(:invoice_id)
+    expect(data[:id]).to eq(tran_1.id.to_s)
+    expect(data[:id]).to_not eq(tran_2.id.to_s)
+  end
+
+  it 'can find a transaction by credit_card_number' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+    tran_1 = create(:transaction, credit_card_number: "4654405418249632", invoice_id: invoice.id)
+    tran_2 = create(:transaction, invoice_id: invoice.id)
+
+    get "/api/v1/transactions/find?#{tran_1.credit_card_number}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to have_key(:credit_card_number)
+    expect(data[:attributes]).to have_key(:credit_card_expiration_date)
+    expect(data[:attributes]).to have_key(:result)
+    expect(data[:attributes]).to have_key(:invoice_id)
+    expect(data[:id]).to eq(tran_1.id.to_s)
+    expect(data[:id]).to_not eq(tran_2.id.to_s)
+  end
+
+  it 'can find a transaction by result' do
+    customer = create(:customer)
+    merchant = create(:merchant)
+    invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
+    tran_1 = create(:transaction, result: "success", invoice_id: invoice.id)
+    tran_2 = create(:transaction, invoice_id: invoice.id)
+
+    get "/api/v1/transactions/find?#{tran_1.result}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:attributes]).to have_key(:credit_card_number)
+    expect(data[:attributes]).to have_key(:credit_card_expiration_date)
+    expect(data[:attributes]).to have_key(:result)
+    expect(data[:attributes]).to have_key(:invoice_id)
+    expect(data[:id]).to eq(tran_1.id.to_s)
+    expect(data[:id]).to_not eq(tran_2.id.to_s)
+  end
+end

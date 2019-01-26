f6eb9e49b2ef78718aebf3e988d796eb75a215b4
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-26 14:37:34 -0700

    can find all by customer attributes

--- app/controllers/api/v1/customers/find_controller.rb
+++ app/controllers/api/v1/customers/find_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::Customers::FindController < ApplicationController
 
+  def index
+    render json: CustomerSerializer.new(Customer.where(customer_params))
+  end
+
+
   def show
     render json: CustomerSerializer.new(Customer.find_by(customer_params))
   end

2ac7b3a6d76bd0d79c75c5d12353a6e8489bdeea
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-26 13:02:22 -0700

    can find customer by attributes

--- spec/requests/api/v1/customers/customer_find_spec.rb
+++ spec/requests/api/v1/customers/customer_find_spec.rb
@@ -34,4 +34,55 @@ describe 'customer find request' do
     expect(data).to have_key(:attributes)
     expect(data[:id]).to eq(cust.id.to_s)
   end
+
+  it 'finds customer by last_name' do
+    create_list(:customer, 2)
+    cust = Customer.first
+
+    get "/api/v1/customers/find?#{cust.last_name}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:id]).to eq(cust.id.to_s)
+  end
+
+  it 'finds customer by created_at' do
+    create_list(:customer, 2)
+    cust = Customer.first
+
+    get "/api/v1/customers/find?#{cust.created_at}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:id]).to eq(cust.id.to_s)
+  end
+
+  it 'finds customer by updated_at' do
+    create_list(:customer, 2)
+    cust = Customer.first
+
+    get "/api/v1/customers/find?#{cust.updated_at}"
+
+    expect(response).to be_successful
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    expect(parsed).to have_key(:data)
+
+    data = parsed[:data]
+    expect(data).to have_key(:id)
+    expect(data).to have_key(:type)
+    expect(data).to have_key(:attributes)
+    expect(data[:id]).to eq(cust.id.to_s)
+  end
 end

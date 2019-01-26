a33df893970257b6ef3f0cbe36a5bedb2834376d
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-26 16:53:40 -0700

    can find invoices based on the customer

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       namespace :customers do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
+        get '/:id/invoices', to: 'customer_invoices#show'
       end
       resources :customers, only: [:index, :show]
 

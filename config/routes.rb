f279dc73f72b8b4824a8199ebcc147d6e5dbf457
Nick Lindeberg <lindebergnick@gmail.com>
2019-01-27 13:55:52 -0700

    can find invoices for a merchant

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
       namespace :merchants do
         get 'find', to: 'find#show'
         get '/find_all', to: 'find#index'
+        get '/:id/invoices', to: 'merchant_invoices#show'
       end
       resources :merchants, only: [:index, :show]
 
